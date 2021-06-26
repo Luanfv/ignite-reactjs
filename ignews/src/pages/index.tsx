c7c8cd14f7c96cdcd651e0bc7694abbe6d7f835a
Luan <luanfrancavieira@gmail.com>
2021-06-26 17:11:28 -0300

    add: sass

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,7 +1,11 @@
+import styles from '../styles/home.module.scss';
+
 function Home() {
-    return (
-        <h1>Hello world!</h1>
-    );
+  return (
+    <h1 className={styles.title}>
+      Hello <span>world</span>!
+    </h1>
+  );
 }
 
 export default Home;
