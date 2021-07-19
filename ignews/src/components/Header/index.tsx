56aa7b618a53034cc0281b8ccb8492f1662873ee
Luan <luanfrancavieira@gmail.com>
2021-07-18 21:49:01 -0300

    add: navigation

--- ignews/src/components/Header/index.tsx
+++ ignews/src/components/Header/index.tsx
@@ -1,17 +1,28 @@
 /* eslint-disable @next/next/no-img-element */
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+
+import { ActiveLink } from '../ActiveLink';
+
 import { SignInButton } from './components/SignInButton';
 
 import styles from './styles.module.scss';
 
 export function Header() {
+  const { asPath } = useRouter();
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
 
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a className={asPath === '/' ? styles.active : ''}>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active} prefetch>
+            <a className={asPath === '/posts' ? styles.active : ''}>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
