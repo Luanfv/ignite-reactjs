86b312e2452369e020ac7cc17a6324d642502ccb
Luan <luanfrancavieira@gmail.com>
2021-08-07 21:38:52 -0300

    feat: header responsive

--- dashgo/src/components/Header/Profile.tsx
+++ dashgo/src/components/Header/Profile.tsx
@@ -1,16 +1,22 @@
 import { Flex, Text, Box, Avatar } from '@chakra-ui/react';
 
-export function Profile() {
+interface IProfileProps {
+  showProfileData: boolean;
+}
+
+export function Profile({ showProfileData }: IProfileProps) {
   return (
     <Flex
       align="center"
     >
-      <Box mr="4" textAlign="right">
-        <Text>Luan Vieira</Text>
-        <Text color="gray.300" fontSize="small">
-          luan@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Luan Vieira</Text>
+          <Text color="gray.300" fontSize="small">
+            luan@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar
         size="md"
