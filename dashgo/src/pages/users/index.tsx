a9ab9760dab6bddb1606cd029af0eb7c540f01f0
Luan <luanfrancavieira@gmail.com>
2021-09-25 18:48:13 -0300

    add: mirage and faker

--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react';
 import {
   Box,
   Button,
@@ -26,6 +27,12 @@ export default function UserList() {
     base: false,
     lg: true,
   });
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/users')
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
 
   return (
     <Box>
